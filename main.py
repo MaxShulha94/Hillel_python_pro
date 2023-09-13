4d91e9c40c7db860b9a1f23e232a79bd51270cf4
Shulha Maksym <113543743+MaxShulha94@users.noreply.github.com>
2023-09-13 20:12:04 +0300

    Add files via upload
    Виконання другої функції в ДЗ

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 def parse(query: str) -> dict:
-    return {}
+    query_dict = {}
+
+    if 'name' in query:
+        query = (query.split('?')[1])
+        if query:
+            keys_values = query.split('&')
+            for items in keys_values:
+                if items == '':
+                    keys_values.remove(items)
+                else:
+                    key, value = items.split('=')
+
+                    query_dict[key] = value
+            return query_dict
+    else:
+        return query_dict
 
 
 if __name__ == '__main__':
@@ -11,7 +26,20 @@ if __name__ == '__main__':
 
 
 def parse_cookie(query: str) -> dict:
-    return {}
+    parse_dict = {}
+    if query == '':
+        return {}
+    else:
+        query = (query.split(';'))
+        for items in query:
+            if items == '':
+                query.remove(items)
+            else:
+                symb = items.find('=')
+                key = items[:symb]
+                value = items.replace('=', ' ', 1).split(' ')[1]
+                parse_dict[key] = value
+        return parse_dict
 
 
 if __name__ == '__main__':
